e45b8b85059412479b5474e7a09c8176b1dc1296
lers145 <lers145@gmail.com>
2021-08-22 21:24:17 -0300

    TP02 Hardware

---
+++ TP02/Doxygen/html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "amp",
+  1: "m",
+  2: "m",
+  3: "ap"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Arquivos",
+  2: "Funções",
+  3: "Variáveis"
+};
+
